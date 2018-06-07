d4eae0168465933ba359b78e1abc26bf11476158
psmzakaria <psmzakaria@gmail.com>
2018-06-07 10:49:15 +0800

    New Edit 10:49

---
+++ script.js
@@ -0,0 +1,58 @@
+// alert("Hello");
+// alert('Hello User');
+// console.log("Hello MISter")
+// console.log(20+21)
+// console.log("20"+"21")
+// console.log(`Hello ,
+
+
+
+
+
+// Class`)
+
+// console.log(true)
+// console.log(false)
+
+
+// const myVariable = 1;
+// const myString = "hello";
+// const myBoolean = true;
+
+// alert(myVariable)
+
+// const bodyClass = document.body.className;
+
+function updateClassToSmaller()
+{
+
+    document.body.className='smaller';
+
+}
+
+function updateClassToBigger()
+{
+
+    document.body.className='Bigger';
+
+}
+
+
+
+function updateClass(className)
+{
+
+document.body.className= className;
+
+}
+// num1 =10 ;
+// if (num1<0)
+// {return true;
+
+// }
+
+// Else
+// {
+// return false;
+
+// }
